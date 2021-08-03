041b4114bf829a9bbcb828937bd56e1583b0ad88
Alex <Alex@DESKTOP-716HUTS>
2021-08-03 13:55:45 +0200

    express update

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'build')));
+const port = process.env.PORT || 5000;
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+app.use(express.static('build'));
+app.get('/', (req, res) => {
+    req.sendFile(path.resolve(__dirname, 'build', 'index.html'))
+})
 
-app.listen(9000);
 
-console.log("App listening on port: 9000")
+app.listen(port, (err) => {
+    if (err) return console.log(err);
+    console.log('Server running on port: ', port)
+})
